89b25dbbe363624ee09b261395757b515a53304e
Aaron Van Prooyen <aaron.vanprooyen@ostusa.com>
2015-09-30 16:05:12 -0400

    test fix

--- src/com/ostusa/ClientSideScripts.java
+++ src/com/ostusa/ClientSideScripts.java
@@ -22,4 +22,6 @@ public class ClientSideScripts {
 	//public static final String FindAllRepeaterRows = "var using = arguments[0] || document; var repeater = arguments[1]; var rows = []; var prefixes = ['ng-', 'ng_', 'data-ng-', 'x-ng-', 'ng\\:']; for (var p = 0; p < prefixes.length; ++p) { var attr = prefixes[p] + 'repeat'; var repeatElems = using.querySelectorAll('[' + attr + ']'); attr = attr.replace(/\\/, ''); for (var i = 0; i < repeatElems.length; ++i) { if (repeatElems[i].getAttribute(attr).indexOf(repeater) != -1) { rows.push(repeatElems[i]); } } } for (var p = 0; p < prefixes.length; ++p) { var attr = prefixes[p] + 'repeat-start'; var repeatElems = using.querySelectorAll('[' + attr + ']'); attr = attr.replace(/\\/, ''); for (var i = 0; i < repeatElems.length; ++i) { if (repeatElems[i].getAttribute(attr).indexOf(repeater) != -1) { var elem = repeatElems[i]; while (elem.nodeType != 8 || !(elem.nodeValue.indexOf(repeater) != -1)) { if (elem.nodeType == 1) { rows.push(elem); } elem = elem.nextSibling; } } } } return rows;";
 
 
+
+
 }
